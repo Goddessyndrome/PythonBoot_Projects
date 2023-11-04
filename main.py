dd9c19db2f3b3ff73cb069e951034b89816b2bbf
Goddessyndrome <123863777+Goddessyndrome@users.noreply.github.com>
2023-11-05 02:05:58 +0530

    Add files via upload
    # DAY 1 - Band Name Generator

---
+++ main.py
@@ -0,0 +1,8 @@
+# PROJECT 1
+# BAND NAME GENERATOR
+
+print("Hello")
+city = input("Which city you grew in?\n")
+pet = input("What is name of your pet\n")
+print("Your band name is " + city + " " + pet)
+
